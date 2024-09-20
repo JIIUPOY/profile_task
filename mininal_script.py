7756f08be02a8f3e050c4a08697c742fbcc89e28
_Vladislav_ <vbatyshkin@mail.ru>
2024-09-20 14:48:37 +0300

    Add types

--- mininal_script.py
+++ mininal_script.py
@@ -2,21 +2,22 @@ import os
 import pandas as pd
 import sys
 from datetime import datetime, timedelta
+from typing import Union
 
-def act_count(dt, act):
+def act_count(dt: pd.DataFrame, act: str) -> pd.Series:
       return dt[dt['action'] == act].groupby('email').size()
 
-d = 7
-date_all = datetime.strptime(str(sys.argv[1]), '%Y-%m-%d').date()
-b_date = date_all - timedelta(days= d)
+d: int = 7
+date_all: datetime = datetime.strptime(str(sys.argv[1]), '%Y-%m-%d').date()
+b_date: datetime = date_all - timedelta(days= d)
 
-end_cols = ['email', 'create_count', 'read_count', 'update_count', 'delete_count']
+end_cols: list[str] = ['email', 'create_count', 'read_count', 'update_count', 'delete_count']
 
 
-dir_name_out = 'output'
-dir_name_in = 'input'
+dir_name_out: str = 'output'
+dir_name_in: str = 'input'
 
-data = pd.DataFrame()
+data: pd.DataFrame = pd.DataFrame()
 for index in range(d):
       dt = pd.read_csv(#добавить проверку, что есть файлы до определенной даты, иначе выводить ошибку с предложением на вывод минимальной даты
             os.path.join(dir_name_in, f'{(b_date + timedelta(days= index))}.csv'),
@@ -24,15 +25,15 @@ for index in range(d):
       data = pd.concat([data, dt])
 
 
-data['dt'] = pd.to_datetime(data['dt']).dt.date
-data = data.sort_values(by=['email', 'dt'])
+data['dt']: pd.Series = pd.to_datetime(data['dt']).dt.date
+data: pd.DataFrame = data.sort_values(by=['email', 'dt'])
 
-create_count = act_count(data, 'CREATE')
-read_count = act_count(data, 'READ')
-update_count = act_count(data, 'UPDATE')
-delete_count = act_count(data, 'DELETE')
+create_count: pd.Series = act_count(data, 'CREATE')
+read_count: pd.Series = act_count(data, 'READ')
+update_count: pd.Series = act_count(data, 'UPDATE')
+delete_count: pd.Series = act_count(data, 'DELETE')
 
-end_data = pd.DataFrame({
+end_data: pd.DataFrame = pd.DataFrame({
       'create_count' : create_count,
       'read_count' : read_count,
       'update_count' : update_count,
@@ -43,5 +44,5 @@ end_data = pd.DataFrame({
 if not os.path.exists(dir_name_out):
       os.makedirs(dir_name_out)
 
-path = os.path.join(dir_name_out, f'{date_all}.csv')
+path: str = os.path.join(dir_name_out, f'{date_all}.csv')
 end_data.to_csv(path, index= False)

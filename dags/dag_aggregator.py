b94b3ff355204cc63ec535dea1162a489c0cb5ba
_Vladislav_ <vbatyshkin@mail.ru>
2024-09-24 23:21:23 +0300

    Изменено название функции внутри DAG - count_func(), исправлен запуск на ежедневный в 7:00

--- dags/dag_aggregator.py
+++ dags/dag_aggregator.py
@@ -4,7 +4,7 @@ from airflow.decorators import task
 from script.aggregate_count import DataAggregator
 
 with DAG(
-        'aggr_count',
+        'aggregation_count',
         default_args={
             'depends_on_past': False,
             'retries': 1,
@@ -12,13 +12,13 @@ with DAG(
         },
         description='Вычисление агрегированных показателей за 7 дней',
         schedule_interval='0 7 * * *',
-        start_date=datetime(2024, 9, 23, 10, 0),
+        start_date=datetime(2024, 9, 25, 5, 0),
         catchup=False,
 ) as dag:
-    @task(task_id='my_dag')
-    def new_func(exec_date: str):
+    @task(task_id='agg_dag')
+    def count_func(exec_date: str):
         required_date = datetime.strptime(exec_date.strip(), "%Y-%m-%d")
         DataAggregator(date_all=required_date).create_data()
 
 
-    new_func(' {{ ds }} ')
+    count_func(' {{ ds }} ')
